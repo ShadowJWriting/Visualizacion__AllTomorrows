a86c5fae90629a00491daa43cd412966f5417171
ShadowJWriting <pepitosamuelelpro@gmail.com>
2025-05-05 23:44:41 -0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,7 @@ function focusGraph(el) {
   document.querySelectorAll('.graph').forEach(g => g.classList.remove('active'));
   el.classList.add('active');
 }
+
+function navigate(direction) {
+  alert(`Navegar a la pestaña: ${direction}`);
+}
